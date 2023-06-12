b40996c971d89f1492d70f8cc01f53cad44d4c54
Pierre Teodorsco <p.teodoresco@gmail.com>
2023-06-12 08:04:06 +0200

    yahou

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # Description: This file contains the implementation of the Stable Marriage Problem
 # Authors: Eliot Colomb & Pierre Teodoresco
 
-import random
-
-
 # Stable Marriage Problem with Gale-Shapley Algorithm
 def stable_marriage(students_prefs, colleges_prefs, slots_per_college):
     students_prefs = {student: preferences for student, preferences in students_prefs.items()}
@@ -40,33 +37,22 @@ def stable_marriage(students_prefs, colleges_prefs, slots_per_college):
 
     return student_engaged, unmatched_students
 
-
-def generate_random_data(num_students, num_colleges, slots_per_college):
-    students = {}
-    colleges = {}
-    college_slots = {}
-
-    for i in range(num_students):
-        student_name = f"Student{i + 1}"
-        student_prefs = random.sample(range(1, num_colleges + 1), num_colleges)
-        students[student_name] = student_prefs
-
-    for i in range(num_colleges):
-        college_name = i + 1
-        college_prefs = random.sample(range(1, num_students + 1), num_students)
-        colleges[college_name] = college_prefs
-        college_slots[college_name] = slots_per_college
-
-    return students, colleges
-
-
 # Example usage
 def main():
-    num_students = 10
-    num_colleges = 4
-    slots_per_college = 2
+    students = {
+        'Emily': ['Harvard', 'MIT', 'Stanford'],
+        'Marc': ['Harvard', 'Stanford', 'MIT'],
+        'John': ['Stanford', 'Harvard', 'MIT'],
+        'Kylian': ['Stanford', 'MIT', 'Harvard']
+    }
 
-    students, colleges = generate_random_data(num_students, num_colleges, slots_per_college)
+    colleges = {
+        'Harvard': ['Emily', 'Marc', 'John', 'Kylian'],
+        'Stanford': ['Emily', 'Marc', 'John', 'Kylian'],
+        'MIT': ['Emily', 'Marc', 'John', 'Kylian']
+    }
+
+    slots_per_college = 1
 
     result, unmatched_students = stable_marriage(students, colleges, slots_per_college)
     for student, college in result.items():
