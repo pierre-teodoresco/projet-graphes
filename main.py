0bdfd8a4060f8969ceb5d5bc2adeadffcfa0a70a
pierre-teodoresco <p.teodoresco@gmail.com>
2023-06-20 14:39:35 +0200

    arguments de script

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# Authors : Eliot Colomb & Pierre Teodoresco
-
+import argparse
 from gale_shapley import gale_shapley
-
 import json
 
-def main():
+def main(json_file, studentBiding):
+    # Convertir la valeur de l'argument en booléen
+    studentBiding = studentBiding.lower() == 'true'
+
     # Charger les données du fichier JSON
-    with open('tests/test_3.json') as file:
+    with open(json_file) as file:
         data = json.load(file)
 
     # Appeler la fonction gale_shapley avec les données du fichier JSON
@@ -14,7 +15,7 @@ def main():
         data['students_preferences'],
         data['schools_preferences'],
         data['school_seats'],
-        False
+        studentBiding
     )
 
     # Afficher les résultats
@@ -26,4 +27,15 @@ def main():
     print(students_without_school)
 
 if __name__ == '__main__':
-    main()
+    # Créer un analyseur d'arguments
+    parser = argparse.ArgumentParser(description='Gale-Shapley Script')
+    
+    # Ajouter les arguments
+    parser.add_argument('json_file', type=str, help='Chemin vers le fichier JSON')
+    parser.add_argument('--studentBiding', type=str, default='True', help='True si eleves proposent, False sinon')
+    
+    # Parser les arguments de la ligne de commande
+    args = parser.parse_args()
+
+    # Appeler la fonction main avec les arguments fournis
+    main(args.json_file, args.studentBiding)
